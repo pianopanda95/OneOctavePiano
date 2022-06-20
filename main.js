d0abfea80bbb24b42c2f87e4744ec5c17d204cdc
pianopanda95 <kramerfanni95@gmail.com>
2022-06-20 17:13:50 +0200

    add animation

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 window.addEventListener('load', () => {
     
-    // const keys = document.querySelectorAll('.key');
-    // console.log(keys);
-
     window.addEventListener('keydown', (e) =>{
         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const key = document.querySelector(`.white[data-key="${e.keyCode}"]`);
-        console.log(audio, key)
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-        if (audio) {
-            audio.currentTime = 0;
-            audio.play();
+        if (!audio) {
+            return;
         }
 
-        // key.setAttribute('active', 'active');
+        audio.currentTime = 0;
+        audio.play();
 
+        key.classList.add('pressed');
+        setTimeout(() => {key.classList.remove('pressed')}, '200');
     });
-
-  
 });
